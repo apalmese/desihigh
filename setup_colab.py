259a4553928253ad042c84f76bfc9a212984e50b
Antonella Palmese <antonella.palmese@gmail.com>
2022-11-06 22:34:43 -0500

    Update setup_colab.py

--- setup_colab.py
+++ setup_colab.py
@@ -5,38 +5,36 @@ import subprocess
 
 drivepath='/content/drive/'
 
-if 'COLAB_GPU' in os.environ:
-    from   google.colab import drive
 
-    
-    # 'Hmmm, seems you\'re not in colab :)  Try again later.'
-    drive.mount(drivepath, force_remount=True)
+from   google.colab import drive
 
-    mydrive = drivepath + '/MyDrive/'
 
-    os.chdir(mydrive)
-    
+# 'Hmmm, seems you\'re not in colab :)  Try again later.'
+drive.mount(drivepath, force_remount=True)
+
+mydrive = drivepath + '/MyDrive/'
+
+os.chdir(mydrive)
+
+try:
+    import  desihigh
+
+except:
+    print('Failed to import desihigh; Cloning.')
+
+    print('git clone https://github.com/michaelJwilson/desihigh.git --depth=1')
+
+    subprocess.run('git clone https://github.com/michaelJwilson/desihigh.git --depth=1', shell=True, check=True)    
+
     try:
+        sys.path.append(mydrive)
+
         import  desihigh
-        
-    except:
-        print('Failed to import desihigh; Cloning.')
 
-        print('git clone https://github.com/michaelJwilson/desihigh.git --depth=1')
-        
-        subprocess.run('git clone https://github.com/michaelJwilson/desihigh.git --depth=1', shell=True, check=True)    
+        print('Successfully cloned DESI High to Google Drive.')
 
-        try:
-            sys.path.append(mydrive)
-            
-            import  desihigh
+    except Exception as EE:
+        emessage = 'Failed to setup DESI High @ colab.  Please create a ticket at https://github.com/michaelJwilson/desihigh.git and include:\n\n{}\n\n{}'.format(EE, sys.path)
 
-            print('Successfully cloned DESI High to Google Drive.')
+        raise  RuntimeError(emessage)
 
-        except Exception as EE:
-            emessage = 'Failed to setup DESI High @ colab.  Please create a ticket at https://github.com/michaelJwilson/desihigh.git and include:\n\n{}\n\n{}'.format(EE, sys.path)
-
-            raise  RuntimeError(emessage)
-
-else:
-    print('It appears you are not on Google Colab (!)')
